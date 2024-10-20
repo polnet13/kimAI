02f084f24a071402bc9800ae8ba08a2b2d7bd2c5
polnet13 <prudent13@gmial.com>
2024-10-20 10:57:47 +0900

    singo tab 수정

--- views/tab/singo.py
+++ views/tab/singo.py
@@ -210,22 +210,6 @@ class Chuldong(Ui_Form, QWidget):
             QMessageBox.critical(self, "파일 오류", '엑셀 파일 2개를 업로드하세요')
         self.signalSingo.emit(1)
     
-    
-    ## 외부 코드 실행
-    def auto_register(self):
-        try:
-            print('-------auto_register 실행---------')
-            # 외부 코드 실행
-            print('외부코드 실행 시작')
-            if DT.df_main is None:
-                print(f'self.result 값이 없음')
-            else:
-                self.ex_code()
-                print('외부코드 실행 끝')
-                
-        except Exception as e:
-            print(e)
- 
 
     ## 선택된 행을 삭제합니다.
     def delete_row(self):
@@ -264,15 +248,31 @@ class Chuldong(Ui_Form, QWidget):
         # 루프 돌면서 df -> 표에 삽입
         # 다른이름으로 저장
     
+    
+    ## 외부 코드 실행
+    def auto_register(self):
+        print('-------auto_register 실행---------')
+        # 외부 코드 실행
+        print('외부코드 실행 시작')
+        if DT.df_main is None:
+            print(f'DT.df_main 값이 없음')
+        else:
+            self.macro_code()
+            print('매크로 코드 실행 끝')
 
-    def ex_code(self):
+            
+ 
+
+    def macro_code(self):
         df = DT.df_main
         # df.columns = ['접수번호','신고내용','종결내용','종결보고자','사건번호','지령시간','지령time','코드','종결']
         time.sleep(1)
-        # 도착시간 계산
-        poi = pyautogui.position()
-        print(poi)
-        print(df.head())
+        # 임의등록 버튼 클릭
+
+        # 접수번호 클릭
+        # 입력
+        # 등록
+
         # df['도착시간'] = df['접수시간'].apply(tools.plus5min)
         df['도착시간'] = pd.to_datetime(df['도착시간'], format='%Y-%m-%d %H:%M:%S')
         df['도착date'] = df['도착시간'].dt.strftime('%Y-%m-%d')
@@ -280,130 +280,106 @@ class Chuldong(Ui_Form, QWidget):
         receipt_num_list = df['접수번호'].tolist()
         arrival_time_date = df['도착date'].tolist()
         arrival_time_list = df['도착time'].tolist()
-        # 임의동록 버튼 좌표 생성
-        try:
-            img_path = os.path.join(DT.BASE_DIR, 'rsc', 'singo', 'target.jpg') 
-            location = pyautogui.locateOnScreen(img_path, confidence= 0.9)
-            location = pyautogui.center(location)
-            print(f'임의등록 버튼: {location}')
-        except TypeError:
-            location = DT.btn_point_1  # 임의등록 좌표
-            pyautogui.alert(f'위치값 인식을 위해 ctrl + 0(숫자) 입력해서 익스플로러창 100%로 맞춰주삼 {location}')
-        except Exception as e:
-            print(e)
-        # 접수번호 입력란 좌표 초기화
-        receipt_num_xy = None
+        # 임의동록(register) 버튼 좌표 생성
+        register = DT.btn_point_1  # 임의등록 좌표
         # 증빙구분 좌표 초기화
         jb_xy = None
         for receipt_num, arrival_date, arrival_time in zip(receipt_num_list, arrival_time_date, arrival_time_list):
-            print(receipt_num, arrival_date, arrival_time)
+            print('임의등록 버튼 클릭!!')
+            pyautogui.moveTo(register, duration=0.2)
+            pyautogui.click()
+            time.sleep(1)
+            # 접수번호 입력란 좌표 초기화
             try:
-                # 임의등록버튼 클릭
-                print('임의등록 버튼 클릭!!')
-                pyautogui.moveTo(location, duration=0.2)
-                pyautogui.click()
-                time.sleep(1)
-                # 접수번호 좌표 있을 경우 좌표생성 패스
-                if receipt_num_xy:
-                    pass
-                # 접수번호 좌표 없을 경우 좌표 생성
-                else:
-                    try:
-                        img_path = os.path.join(DT.BASE_DIR, 'rsc', 'singo', '1.jpg')
-                        receipt_num_xy = pyautogui.locateOnScreen(img_path, confidence= 0.80)
-                        print(f'센터값1: {receipt_num_xy}')
-                        receipt_num_xy = pyautogui.center(receipt_num_xy)
-                        print(f'센터값2: {receipt_num_xy}')
-                    except TypeError:
-                        pyautogui.alert('접수번호 이미지를 인식하지 몬함 800*350')
-                        receipt_num_xy = DT.btn_point_2  # 접수번호
-                    except Exception as e:
-                        print(e)
-                pyautogui.moveTo(receipt_num_xy, duration=0.2)
-                pyautogui.click()
-                pyautogui.typewrite(f'{receipt_num}', interval = 0.01)
-                # 이미 등록된 경우 패스하기
-                img_path4 = os.path.join(DT.BASE_DIR, 'rsc', 'singo', '4.jpg')  # 이미 등록된  
-                img_path6 = os.path.join(DT.BASE_DIR, 'rsc', 'singo', '6.jpg')  # 등록 가능한  
-                result4 = False
-                result6 = False
-                while True:
-                    result4 = pyautogui.locateOnScreen(img_path4, confidence= 0.9)
-                    if result4:
-                        # 이미 등록된
-                        result = 'stop'
-                        break
-                    result6 = pyautogui.locateOnScreen(img_path6, confidence= 0.9)
-                    if result6:
-                        # 등록 가능한
-                        result = 'go'
-                        break
-                    time.sleep(0.1)
-                if result == 'stop':
-                    # 이미 등록된 경우 패스 하는 코드임
-                    tools.tap_n(14)
-                    pyautogui.press('enter')
-                    time.sleep(0.1)
-                    pyautogui.press('enter')
-                    time.sleep(0.1)
-                    continue
-                # 도착일 입력: 탭6번 후 입력
-                tools.tap_n(6)
-                pyautogui.typewrite(f'{arrival_date}', interval = 0.02)
-                # 도착시간 입력
-                pyautogui.press('tab')
-                pyautogui.typewrite(f'{arrival_time}', interval = 0.02)
-                # 사유 작성
-                tools.tap_n(2)
-                pyperclip.copy('출동수당 누락으로 입력 함')
-                pyautogui.hotkey('ctrl', 'v')
-                # pyautogui.typewrite('출동수당 누락으로 입력 함', interval = 0.06)
-                time.sleep(0.1)
-                # 증빙구분선택 좌표 있을 경우 좌표생성 패스
-                if jb_xy:
-                    pass
-                # 증빙구분선택 좌표 없을 경우 좌표 생성
-                else:
-                    try:
-                        img_path = os.path.join(DT.BASE_DIR, 'rsc', 'singo', '3.jpg')  
-                        jb_xy = pyautogui.locateOnScreen(img_path, confidence= 0.98)
-                        print(f'센터값1: {jb_xy}')
-                        jb_xy = pyautogui.center(jb_xy)
-                        print(f'센터값2: {jb_xy}')
-                    except TypeError:
-                        pyautogui.alert('증빙구분(3) 이미지를 인식하지 몬함')
-                    except Exception as e:
-                        print(e)
-                # 증빙구분 클릭
-                pyautogui.moveTo(jb_xy, duration=0.2)
-                pyautogui.click()
-                time.sleep(0.2)
-                # 입력버튼 좌표 있을 경우 좌표생성 패스
-                tools.tap_n(3)
-                pyautogui.press('enter')
-                time.sleep(0.7)
-                enter_1 = False
-                enter_2 = False
-                while True:
-                    enter_1 = pyautogui.locateOnScreen(os.path.join(DT.BASE_DIR, 'rsc', 'singo', 'enter_1.jpg'), confidence= 0.9)   
-                    if enter_1:
-                        print('임의등록을 하겠습니까? => 버튼 확인함')
-                        break
-                    time.sleep(0.2)
-                pyautogui.press('enter')
-                while True:
-                    enter_2 = pyautogui.locateOnScreen(os.path.join(DT.BASE_DIR, 'rsc', 'singo', 'enter_2.jpg'), confidence= 0.9)
-                    if enter_2:
-                        print('저장되었습니다. => 버튼 확인함')
-                        break
-                    time.sleep(0.2)
-                pyautogui.press('enter')
-                time.sleep(0.2)                
+                img_path = os.path.join(DT.BASE_DIR, 'rsc', 'singo', '1.jpg')
+                receipt_num_xy = pyautogui.locateOnScreen(img_path, confidence= 0.80)
+                receipt_num_xy = pyautogui.center(receipt_num_xy)
+                print(f'센터값2: {receipt_num_xy}')
             except Exception as e:
+                pyautogui.alert('접수번호 이미지를 인식하지 몬함 800*350')
+                receipt_num_xy = DT.btn_point_2  # 접수번호
                 print(e)
-                print('위의 오류로 루프 종료함')
-                break
-                
+            pyautogui.moveTo(receipt_num_xy, duration=0.2)
+            pyautogui.click()
+            pyautogui.typewrite(f'{receipt_num}', interval = 0.01)
+            # 이미 등록된 경우 패스하기
+            img_path4 = os.path.join(DT.BASE_DIR, 'rsc', 'singo', '4.jpg')  # 이미 등록된  
+            img_path6 = os.path.join(DT.BASE_DIR, 'rsc', 'singo', '6.jpg')  # 등록 가능한  
+            result4 = False
+            result6 = False
+            while True:
+                try:
+                    result4 = pyautogui.locateOnScreen(img_path4, confidence=0.8)
+                except pyautogui.ImageNotFoundException:
+                    print("화면에서 이미지를 찾을 수 없습니다.")
+                if result4:
+                    # 이미 등록된
+                    result = 'stop'
+                    break
+                result6 = pyautogui.locateOnScreen(img_path6, confidence= 0.9)
+                if result6:
+                    # 등록 가능한
+                    result = 'go'
+                    break
+                time.sleep(0.1)
+            if result == 'stop':
+                # 이미 등록된 경우 패스 하는 코드임
+                tools.tap_n(14)
+                pyautogui.press('enter')
+                time.sleep(0.1)
+                pyautogui.press('enter')
+                time.sleep(0.1)
+                continue
+            # 도착일 입력: 탭6번 후 입력
+            tools.tap_n(6)
+            pyautogui.typewrite(f'{arrival_date}', interval = 0.02)
+            # 도착시간 입력
+            pyautogui.press('tab')
+            pyautogui.typewrite(f'{arrival_time}', interval = 0.02)
+            # 사유 작성
+            tools.tap_n(2)
+            pyperclip.copy('출동수당 누락으로 입력 함')
+            pyautogui.hotkey('ctrl', 'v')
+            # pyautogui.typewrite('출동수당 누락으로 입력 함', interval = 0.06)
+            time.sleep(0.1)
+            # 증빙구분선택 좌표 있을 경우 좌표생성 패스
+            if jb_xy:
+                pass
+            # 증빙구분선택 좌표 없을 경우 좌표 생성
+            else:
+                try:
+                    img_path = os.path.join(DT.BASE_DIR, 'rsc', 'singo', '3.jpg')  
+                    jb_xy = pyautogui.locateOnScreen(img_path, confidence= 0.98)
+                    jb_xy = pyautogui.center(jb_xy)
+                    print(f'증빙구분 좌표 값: {jb_xy}')
+                except Exception as e:
+                    pyautogui.alert('증빙구분(3) 이미지를 인식하지 몬함')
+                    print(e)
+            # 증빙구분 클릭
+            pyautogui.moveTo(jb_xy, duration=0.2)
+            pyautogui.click()
+            time.sleep(0.2)
+            # 입력버튼 좌표 있을 경우 좌표생성 패스
+            tools.tap_n(3)
+            pyautogui.press('enter')
+            time.sleep(0.7)
+            enter_1 = False
+            enter_2 = False
+            while True:
+                enter_1 = pyautogui.locateOnScreen(os.path.join(DT.BASE_DIR, 'rsc', 'singo', 'enter_1.jpg'), confidence= 0.9)   
+                if enter_1:
+                    print('임의등록을 하겠습니까? => 버튼 확인함')
+                    break
+                time.sleep(0.2)
+            pyautogui.press('enter')
+            while True:
+                enter_2 = pyautogui.locateOnScreen(os.path.join(DT.BASE_DIR, 'rsc', 'singo', 'enter_2.jpg'), confidence= 0.9)
+                if enter_2:
+                    print('저장되었습니다. => 버튼 확인함')
+                    break
+                time.sleep(0.2)
+            pyautogui.press('enter')
+            time.sleep(0.2)                    
         pyautogui.alert('끝')
         
 
